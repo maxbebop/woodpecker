9a039c3ee644987b65d33a43bf12283609406fd6
Maxim Kubarskiy <maxim.kubarskiy@microavia.com>
2023-05-14 18:52:36 +0300

    tests

---
+++ internal/configs/config_test.go
@@ -0,0 +1,22 @@
+package config_test
+
+import (
+	"testing"
+	config "woodpecker/internal/configs"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	cfg := config.New("../../slack.config.yml")
+	expected := createConfigMock()
+
+	require.Equal(t, expected, cfg, "config")
+}
+
+func createConfigMock() *config.Config {
+	cfg := config.Config{}
+	cfg.Slack.AppToken = "xxx"
+	cfg.Slack.OAuthToken = "xxx"
+	return &cfg
+}
--- internal/services/chat/service_test.go
+++ internal/services/chat/service_test.go
@@ -1,33 +1,53 @@
 package chatservice_test
 
 import (
-	"context"
+	context "context"
 	"testing"
 
+	config "woodpecker/internal/configs"
+
 	chatservice "woodpecker/internal/services/chat"
+	slackservice "woodpecker/internal/services/slack"
+
+	structlog "github.com/powerman/structlog"
+	"github.com/stretchr/testify/require"
 )
 
 // this is not just a comment but the way to instruct `go generate` command
 // you can run `go generate ./...` in the project root to process all the files across the project
 //go:generate mockery --all --testonly --outpkg chatservice_test --output .
 
+func TestProcessMsg(t *testing.T) {
+
+	mockChatBot := NewChatBot(t)
+
+	outMsg := chatservice.OutMessage{Message: chatservice.Message{}, Type: chatservice.Common, Pretext: "", Error: nil}
+
+	mockChatBot.On("SendMessage", outMsg).Return(nil)
+
+	err := mockChatBot.SendMessage(outMsg)
+
+	require.NoError(t, err, "chatservice SendMessage")
+}
+
 func TestStartChat(t *testing.T) {
-	// ToDo: fix the test to cover at leas something :)
+	log := structlog.New()
+	cfg := config.New("../../../slack.config.yml")
+	chatBot := slackservice.New(cfg, log)
+
+	require.NotNil(t, chatBot, "chatBot")
+
+	mockChatBot := NewChatBot(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// log := structlog.New()
-	inMsgChannel := make(chan chatservice.Message)
-	chatBot := &ChatBot{}
-	chatBot.On("GetMessagesLoop", ctx, inMsgChannel, nil)
-	chatBot.On("SendMessage", chatservice.OutMessage{
-		Message: chatservice.Message{
-			User:    "",
-			Channel: "",
-			Text:    "",
-			Error:   nil,
-		},
-		Type:    chatservice.Common,
-		Pretext: "",
-		Error:   nil})
-	close(inMsgChannel)
+
+	chatChannel := make(chan chatservice.Message)
+	mockChatBot.On("GetMessagesLoop", ctx, chatChannel, log).Return()
+	mockChatBot.GetMessagesLoop(ctx, chatChannel, log)
+
+	mockChatBot.On("Run").Return(nil)
+	mockChatBotErr := mockChatBot.Run()
+	ctx.Done()
+
+	require.NoError(t, mockChatBotErr, "chatservice Run")
 }
